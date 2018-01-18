916986502018740b3afa99c9541350738e9d40d2
ICTKVPJB <ICTKVPJB@ICTKVPJB-PC>
2018-01-18 10:33:47 +0800

    syaza

---
+++ src/pkg2kpd1/Keluasan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2kpd1;
+
+/**
+ *
+ * @author ICTKVPJB
+ */
+public class Keluasan {
+
+  public static void main(String args[])
+  {
+
+   int jejari=20;
+   float pi=3.14159f;
+   
+float luas= jejari*jejari*pi;
+ 
+      System.out.println("Keluasan keseluruhan:"+luas);
+
+     }
+}
---
+++ src/pkg2kpd1/Main.java
@@ -0,0 +1,14 @@
+class Keluasan
+{
+  public static void main(String args[])
+  {
+
+   int jejari=20;
+   float pi=3.14159f;
+   
+float luas= jejari*jejari*pi;
+ 
+      System.out.println("Keluasan keseluruhan:"+luas);
+
+     }
+}
